49aa1178c5f6b5aba08bce0f66c3931f970ec34a
Valery Onishkov <67824716+OnishkovValera@users.noreply.github.com>
2025-12-18 11:29:51 +0300

    Add dependencies for URL query params and forms (#2)
    add button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,36 @@
-import { ThemeProvider } from "@/components/ThemeProvider"
+import {ThemeProvider} from "@/components/ThemeProvider"
 import {ModeToggle} from "@/components/ModeToggle.tsx";
-import { Button } from "./components/ui/button";
+import {Button} from "./components/ui/button";
+import {NuqsAdapter} from 'nuqs/adapters/react-router/v7'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import About from "@/pages/About.tsx";
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <ModeToggle></ModeToggle>
-            <Button>Test Theme</Button>
-        </ThemeProvider>
+        <NuqsAdapter>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <RouterProvider router={
+                    createBrowserRouter([
+                        {
+                            path: "/",
+                            element: <Button>Test Theme</Button>,
+                            loader: () => {
+                                return "testData"
+                            }
+                        },
+                        {
+                            path: "/about",
+                            element: <About></About>,
+                            loader: () => {
+                                return "testData"
+                            }
+                        }
+                    ])
+                }>
+                </RouterProvider>
+                <ModeToggle></ModeToggle>
+            </ThemeProvider>
+        </NuqsAdapter>
     )
 }
 
